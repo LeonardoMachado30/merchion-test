63b37a8d9614002b48f3ab5a5c08606735dab7aa
flmpl <flmp.leonardo@gmail.com>
2025-06-26 23:22:19 -0300

    Inicilizando projeto laravel

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\UserRequest;
+use App\Services\UserService;
+use Illuminate\Routing\Controller;
+
+class UserController extends Controller
+{
+
+}
